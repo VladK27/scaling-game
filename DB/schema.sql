dace4cde5f9657f542a87685f96252b144357a96
VladK27 <karelinvlad.dev@gmail.com>
2025-08-30 22:53:53 +0300

    fix schema.sql

--- DB/schema.sql
+++ DB/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE device_events (
     event_id BIGSERIAL PRIMARY KEY,
     event_time TIMESTAMPTZ NOT NULL,
     battery_level INT NOT NULL CHECK (battery_level BETWEEN 0 AND 100),
-    city VARCHAR(64) NOT NULL,
+    city VARCHAR(128) NOT NULL,
     country TEXT NOT NULL,
     temperature REAL NOT NULL,
     wind_speed REAL NOT NULL,
